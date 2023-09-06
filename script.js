db887265b1fb3e892601ddcdfee52a2c06fe9503
Faberroni <raizal.ashari@gmail.com>
2023-09-06 07:13:40 +0700

    added playerchoice function

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ function getComputerChoice () {
     var hand = ['Rock', 'Paper', 'Scissor']
     var random = hand[(Math.random() * hand.length) | 0]
     console.log(random)
+    return random
 }
 
-getComputerChoice()
+function playerChoice() {
+    let choice = prompt('Pick your hand!')
+    hand = choice.toLowerCase()
+    switch (hand) {
+        case 'rock':
+            console.log('you picked rock');
+            break;
+        case 'paper':
+            console.log('you picked paper');
+            break;
+        case 'scissor':
+            console.log('you picked scissor');
+            break;
+        default:
+            console.error('not a legal choice please pick again');
+            playerChoice();
+    }
+    console.log(hand)
+    return hand
+}
