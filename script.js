3b6332c09cb4ab884677142c07106258fab3d5ae
raizalas <ashari.raizal17@gmail.com>
2024-03-28 09:24:38 +0700

    play game function implemented with 5 rounds

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 let getComputerChoice = function () {
-    var hand = ['rock', 'paper', 'scissor'];
-    var random = hand[(Math.random() * hand.length) | 0];
+    let hand = ['rock', 'paper', 'scissor'];
+    let random = hand[(Math.random() * hand.length) | 0];
+    console.log(random)
     return random;
 }
 
@@ -27,19 +28,59 @@ let playerChoice = function () {
 
 
 let playRound = function (playerHand, computerHand) {
-    console.log(playerHand);
-    console.log(computerHand);
+    console.log(`The player picked: ${playerHand}`);
+    console.log(`The computer picked: ${computerHand}`);
+    let result;
 
     if (playerHand === computerHand ) {
          console.log("it's a draw");
+         result = 'draw';
     } else if (
         (playerHand === "rock" && computerHand === "scissor") ||
         (playerHand === "scissor" && computerHand === "paper") ||
         (playerHand === "paper" && computerHand === "rock")
     ) {
          console.log(`you win`);
+         result = 'win';
     } else {
         console.log(`you lose`);
+        result = 'lose';
     }
+    return result;
 }
 
+
+function playGame() {
+    let roundsPlayed = 0;
+    let playerWins = 0;
+    let computerWins = 0;
+
+    while (roundsPlayed < 5) {
+        game = playRound(playerChoice(), getComputerChoice())
+
+        switch (game) {
+            case 'win':
+                console.log('player wins this round!')
+                playerWins++
+                break
+            case 'lose':
+                console.log('computer wins this round')
+                computerWins++
+            default:
+                console.log('draw')
+        }
+        console.log("")
+        roundsPlayed++;
+    }
+
+    if (playerWins > computerWins) {
+        console.log("Player won this game!")
+    } else if (computerWins > playerWins) {
+        console.log("Computer won this game")
+    } else {
+        console.log("it's a tie")
+    }
+
+}
+
+playGame()
